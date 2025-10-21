a4bdd5dd3292034ba195358a449798bf75c1bde7
sepho706 <sephorayao225@gmail.com>
2025-10-21 14:12:07 +0000

    second commit

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./contact.css";
+import "./Contact.css";
 import contactData from "../data/contact.json";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
